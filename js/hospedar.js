f5e7f4f76aa448fff286a762db4420f3e11fba73
Willian Ferreira dos santos <willian1948@hotmail.com>
2020-11-16 18:29:42 -0300

    6

--- js/hospedar.js
+++ js/hospedar.js
@@ -1,13 +1,36 @@
-const botao = document.querySelector('#botao')
-botao.addEventListener('click',Calcular)
+document.querySelector('form').addEventListener('submit', Exibir)
 
 function Calcular(){
 
 }
 
-function Exibir(){
+function Exibir(evento){
+    evento.preventDefault()
+
+    Calcular();
+
     const result = document.querySelector('#result')
 
-    const div = document.createElement('div')
-    
+
+
+    const table = document.createElement('table')
+
+    const thead = document.createElement('thead')
+
+    const tr = document.createElement('tr')
+
+    const th = document.createElement('th')
+    th.textContent = 'Nome'
+
+    tr.appendChild(th)
+
+    th.textContent = 'Email'
+
+    tr.appendChild(th)
+
+    thead.appendChild(tr)
+
+    table.appendChild(thead)
+
+    result.appendChild(table)
 }
