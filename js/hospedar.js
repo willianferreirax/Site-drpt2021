ede3f7ad5602c156182fe19e76778751a8a6fd0b
Willian Ferreira <willian1948@hotmail.com>
2020-11-16 17:03:40 -0300

    5

---
+++ js/hospedar.js
@@ -0,0 +1,13 @@
+const botao = document.querySelector('#botao')
+botao.addEventListener('click',Calcular)
+
+function Calcular(){
+
+}
+
+function Exibir(){
+    const result = document.querySelector('#result')
+
+    const div = document.createElement('div')
+    
+}
