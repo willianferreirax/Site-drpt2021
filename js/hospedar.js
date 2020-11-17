574f8dc77b120b12471930612e03b72090b3856d
Willian Ferreira <willian1948@hotmail.com>
2020-11-16 22:01:31 -0300

    7

--- js/hospedar.js
+++ js/hospedar.js
@@ -1,5 +1,13 @@
 document.querySelector('form').addEventListener('submit', Exibir)
 
+const campos = [
+    document.querySelector('#name'),
+    document.querySelector('#email'),
+    document.querySelector('#birthday'),
+    document.querySelector('#quantity'),
+    document.querySelector('#trip_date')   
+]
+
 function Calcular(){
 
 }
@@ -10,27 +18,50 @@ function Exibir(evento){
     Calcular();
 
     const result = document.querySelector('#result')
-
-
-
+    
     const table = document.createElement('table')
+    table.id = 'tabela'
 
     const thead = document.createElement('thead')
+    thead.id = 'cabecalho'
 
+    if(!document.querySelector('#cabecalho')){
+        const tr = document.createElement('tr')
+        const tn = ['Nome','Email','Data de nascimento','Quantidade de pessoas','Data da viagem','Local de hospedagem','valor total da hospedagem','valor total do translado','valor final']
+
+        tn.forEach((dados)=>{
+            const th = document.createElement('th')
+            th.textContent = dados
+            tr.appendChild(th)
+        })
+
+        thead.appendChild(tr)
+
+        table.appendChild(thead)
+    }
+    
+    const tbody = document.createElement('tbody')
     const tr = document.createElement('tr')
 
-    const th = document.createElement('th')
-    th.textContent = 'Nome'
+    campos.forEach((dados) => {
+        
+        const td = document.createElement('td')
 
-    tr.appendChild(th)
+        td.textContent = dados.value
 
-    th.textContent = 'Email'
+        tr.appendChild(td)
 
-    tr.appendChild(th)
+    })
 
-    thead.appendChild(tr)
+    const td = document.createElement('td')
 
-    table.appendChild(thead)
+    td.textContent = document.querySelector('input[name=place]:checked').value
+
+    tr.appendChild(td)
+
+    tbody.appendChild(tr)
+
+    table.appendChild(tbody)
 
     result.appendChild(table)
 }
