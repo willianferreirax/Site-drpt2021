e99c5806e718ad92818d4d20580c5745f48e8037
Willian Ferreira <willian1948@hotmail.com>
2020-11-17 16:15:33 -0300

    8

--- js/hospedar.js
+++ js/hospedar.js
@@ -5,29 +5,54 @@ const campos = [
     document.querySelector('#email'),
     document.querySelector('#birthday'),
     document.querySelector('#quantity'),
-    document.querySelector('#trip_date')   
+    document.querySelector('#trip_date'),
 ]
 
 function Calcular(){
+     let val = campos[3].value * 9500
+     campos.push(val)
+     val = campos[3].value * 33500
+     campos.push(val)
+     val = campos[6] + campos[7]
+     campos.push(val)
+}
 
+function DateValidation(){
+    if(Date.now()-Date.parse(campos[2].value) < 568080000000){
+        alert('O responsável deve ser maior de idade (18 anos)')
+        return true;
+    }
+
+    if(Date.parse(campos[4].value) < Date.now()){
+        alert('Não é possível marcar a viagem para a data escolhida\n (A data já passou)')
+        return true;
+    }
 }
 
 function Exibir(evento){
     evento.preventDefault()
 
+    if(DateValidation()){
+        return;
+    }
+
+    campos.push(document.querySelector('input[name=place]:checked'))
     Calcular();
 
     const result = document.querySelector('#result')
     
-    const table = document.createElement('table')
-    table.id = 'tabela'
+    let table = 0;
+    let tbody =0;
 
-    const thead = document.createElement('thead')
-    thead.id = 'cabecalho'
+    if(!document.querySelector('thead')){
+        table = document.createElement('table')
+        table.classList.add('table','table-hover','text-center','table-responsive','table-borderless','shadow')
 
-    if(!document.querySelector('#cabecalho')){
+        const thead = document.createElement('thead')
+        thead.classList.add('bg-primary','text-warning')
+
         const tr = document.createElement('tr')
-        const tn = ['Nome','Email','Data de nascimento','Quantidade de pessoas','Data da viagem','Local de hospedagem','valor total da hospedagem','valor total do translado','valor final']
+        const tn = ['Nome do responsável','Email do responsável','Data de nascimento','Quantidade de pessoas','Data da viagem','Local de hospedagem','Valor total da hospedagem','Valor total do translado','Valor final']
 
         tn.forEach((dados)=>{
             const th = document.createElement('th')
@@ -38,30 +63,38 @@ function Exibir(evento){
         thead.appendChild(tr)
 
         table.appendChild(thead)
+
+        tbody = document.createElement('tbody')
+
+        table.appendChild(tbody)
+    }
+    else{
+        table = document.querySelector('table')
+        tbody = document.querySelector('tbody')
     }
     
-    const tbody = document.createElement('tbody')
     const tr = document.createElement('tr')
 
     campos.forEach((dados) => {
         
         const td = document.createElement('td')
 
-        td.textContent = dados.value
-
+        if(dados instanceof HTMLElement){
+            td.textContent = dados.value
+        }
+        else{
+            td.textContent = dados
+        }
         tr.appendChild(td)
 
     })
 
-    const td = document.createElement('td')
-
-    td.textContent = document.querySelector('input[name=place]:checked').value
-
-    tr.appendChild(td)
-
     tbody.appendChild(tr)
-
+  
     table.appendChild(tbody)
-
+    
     result.appendChild(table)
+
+    this.reset()
+    campos.splice(5,8)
 }
